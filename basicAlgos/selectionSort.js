2c803d7a8f12403f6607bf4eea6fb9d4d2c681d1
noliver-ykc <74501096+noliver-ykc@users.noreply.github.com>
2022-11-25 11:57:38 +0900

    Update selectionSort.js
    just fixed some typos

--- basicAlgos/selectionSort.js
+++ basicAlgos/selectionSort.js
@@ -1,14 +1,14 @@
 function selectionSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    let lowest = i
+    let lowest = i;
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[j] < arr[lowest]) {
-        lowest = j
+        lowest = j;
       }
     }
     if (lowest !== i) {
       // Swap
-      ;[arr[i], arr[lowest]] = [arr[lowest], arr[i]]
+      [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
     }
   }
   return arr
