e1932c144c2e51629904904c3644eb2efa71d39f
Nicole Olliver <noliver.ykc@gmail.com>
2022-12-17 17:30:48 +0900

    get a the value at a certain index with linked lists

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -62,6 +62,16 @@ class SinglyLinkedList{
     this.length++;
     return this;
   }
+  get(index){
+    if(index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while(counter !== index){
+        current = current.next;
+        counter++;
+    }
+    return current;
+  }
 }
 
 var list = new SinglyLinkedList()
