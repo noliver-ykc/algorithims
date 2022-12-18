d515a476668fae77fa93f3a5c17e84e7c0e22e93
Nicole Olliver <noliver.ykc@gmail.com>
2022-12-18 16:07:41 +0900

    updated for ES6

--- dataStructures/singlyLinkedList.js
+++ dataStructures/singlyLinkedList.js
@@ -100,8 +100,8 @@ class SinglyLinkedList{
     if(index < 0 || index >= this.length) return undefined;
     if(index === 0) return this.shift();
     if(index === this.length - 1) return this.pop();
-    var previousNode = this.get(index - 1);
-    var removed = previousNode.next;
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
     previousNode.next = removed.next;
     this.length--;
     return removed;
